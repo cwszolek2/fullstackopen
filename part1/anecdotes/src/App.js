32a3937075d8d5a413869ccb870a6cbdcafb9508
cwszolek2 <cwszolek2@gmail.com>
2022-09-22 16:59:14 -0500

    Final update to Part1 anecdotes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -6,6 +6,28 @@ const Button = (props) => (
   </button>
 );
 
+const Header = (props) => <h1>{props.text}</h1>
+
+const Anecdote = ({text, votes}) => {
+  return (
+    <div>
+      <p>{text}</p>
+      <p>has {votes} votes</p>
+    </div>
+  )
+}
+
+const MostVotes = ({anecdotes, votes}) => {
+  const indexMostVotes = votes.indexOf(Math.max.apply(null, votes));
+  console.log(indexMostVotes);
+  return (
+    <div>
+      <p>{anecdotes[indexMostVotes]}</p>
+      <p>has {votes[indexMostVotes]} votes </p>
+    </div>
+  )
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -17,32 +39,31 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ]
   //Note: there is a way to dynamically declare points using a for loop, but we'll skip that since that doesn't feel like the focus.
-  const randomSelect = () => {
+  const handleRandomSelect = () => {
     setSelected(Math.floor(Math.random()*anecdotes.length));
   }
   
-  const vote = () => {
+  const handleVote = () => {
     const copy = [...votes];
     copy[selected] += 1;
     setVotes(copy);
     // This is a relatively lazy way to handle dealing with the max amount
     // But I thought it was cute since it was another way to use a State to achieve this.
-    if(copy[selected] > copy[max]) setMax(selected);
+    //if(copy[selected] > copy[max]) setMax(selected);
   }
    
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
-  const [max, setMax] = useState(0);
+  //const [max, setMax] = useState(0);
 
   return (
     <div>
-      <h1>Anecdote of the day</h1>
-      <div>{anecdotes[selected]}</div>
-      <div>has {votes[selected]} votes</div>
-      <Button handleClick={vote} text="Vote" />
-      <Button handleClick={randomSelect} text="Random Anecdote" />
-      <h1>Anecdote with most votes</h1>
-      <div>{anecdotes[max]}</div>
+      <Header text="Anecdote of the day" />
+      <Anecdote text={anecdotes[selected]} votes={votes[selected]} />
+      <Button handleClick={handleVote} text="Vote" />
+      <Button handleClick={handleRandomSelect} text="Random Anecdote" />
+      <Header text="Anecdote with the most votes" />
+      <MostVotes anecdotes={anecdotes} votes={votes}/>
     </div>
   )
 }
