e2938fb2bab83e34aac3346e9729238362b0de85
cwszolek2 <cwszolek@hawk.iit.edu>
2025-09-06 15:35:31 -0500

    finished 3.7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,10 @@
+//On 3.8
 const express = require('express')
 const app = express()
 app.use(express.json())
+const morgan = require('morgan')
+app.use(morgan('tiny'))
+
 
 let phonebook = [
     { 
